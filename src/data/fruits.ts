083c291ad82db8c07f752546bbe2c643046f309f
Sezer Esim <sezeresim@gmail.com>
2022-04-22 00:12:58 +0300

    .

---
+++ src/data/fruits.ts
@@ -0,0 +1,47 @@
+export const fruits = [
+  {
+    id: 'apple',
+    name: 'Apple',
+    image: '/images/red-apple.png',
+  },
+  {
+    id: 'banana',
+    name: 'Banana',
+    image: '/images/banana.png',
+  },
+  {
+    id: 'orange',
+    name: 'Orange',
+    image: '/images/orange.png',
+  },
+  {
+    id: 'grape',
+    name: 'Grape',
+    image: '/images/grape.png',
+  },
+  {
+    id: 'cherry',
+    name: 'Cherry',
+    image: '/images/cherry.png',
+  },
+  {
+    id: 'lemon',
+    name: 'Lemon',
+    image: '/images/lemon.png',
+  },
+  {
+    id: 'pineapple',
+    name: 'Pineapple',
+    image: '/images/pineapple.png',
+  },
+  {
+    id: 'watermelon',
+    name: 'Watermelon',
+    image: '/images/watermelon.png',
+  },
+  {
+    id: 'kiwi',
+    name: 'Kiwi',
+    image: '/images/kiwi.png',
+  },
+];
