51e10a5404533dbfd6db61adbcee8d1fd1643aab
Sezer Esim <sezeresim@gmail.com>
2022-05-02 00:08:56 +0300

    Button test

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
     'prettier',
   ],
   rules: {
+    'no-empty-function': 'warn',
+    '@typescript-eslint/no-empty-function': 'warn',
     'no-unused-vars': 'off',
     'no-console': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
--- src/components/atoms/Buttons/Button.test.tsx
+++ src/components/atoms/Buttons/Button.test.tsx
@@ -6,4 +6,9 @@ describe('Button', () => {
     const { container } = render(<Button>Hello</Button>)
     expect(container).toBeInTheDocument()
   })
+  it('should render with variant=danger', () => {
+    const button = render(<Button variant='danger'>Hello</Button>)
+    expect(button.getByRole('button')).toHaveClass('bg-red-800')
+    expect(button.getByText('Hello')).toHaveClass('bg-red-500 hover:bg-red-600')
+  })
 })
